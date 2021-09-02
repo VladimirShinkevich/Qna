78472865527af8d86ff7abb99d1e5a226875f31b
VladimirShinkevich <vladimirshinkevich1988@mail.ru>
2021-09-02 18:49:28 +0300

    Добавлены ассоциации для моделей Вопросов и Ответов.

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should validate_presence_of :body}
+  describe 'associations' do 
+    it { should belong_to :question}
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :body}
+  end
 end
