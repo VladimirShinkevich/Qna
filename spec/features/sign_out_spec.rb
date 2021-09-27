20b205ad0f210200c86cac104162e48dd4085efa
VladimirShinkevich <vladimirshinkevich1988@mail.ru>
2021-09-27 22:04:23 +0300

    Authenticate user can sign out

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'User sign out' do 
+  let(:user) { create(:user) }
+  before { signin(user) }
+
+  scenario 'Authenticated user can sign out' do
+    click_on 'Sign out'
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
