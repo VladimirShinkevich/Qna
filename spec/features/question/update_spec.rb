5eeb9b052c7dd27fe490cc243f1da01bb685a60c
VladimirShinkevich <vladimirshinkevich1988@mail.ru>
2021-10-21 17:42:40 +0300

    The author of the question can add files when editing his question

--- spec/features/question/update_spec.rb
+++ spec/features/question/update_spec.rb
@@ -43,6 +43,27 @@ feature 'User can edit his question' do
         expect(page).to have_selector 'textarea' 
       end
     end
+
+    scenario 'edit question with attached files' do 
+
+      within '.question' do
+        click_on 'Edit question'
+        fill_in 'Your title', with: 'update title'
+        fill_in 'Your question', with: 'update question'
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+
+        click_on 'Save question'
+
+        expect(page).to_not have_content question.title
+        expect(page).to_not have_content question.body
+        expect(page).to have_content 'update title'
+        expect(page).to have_content 'update question'
+        expect(page).to_not have_selector 'textarea'
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 
   scenario 'Authenticated user tries edit other users question' do 
