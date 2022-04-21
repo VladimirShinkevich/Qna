bd12e208f7090e23aac1a1fbe0e8ebb79edb3081
Vladimir <vladimirshinkevich1988@mail.ru>
2022-04-21 17:15:22 +0300

    add smtp key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -119,7 +120,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'http://188.68.220.23' }
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: '188.68.220.23', protocol: "https"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
@@ -129,4 +132,6 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
+
+    config.action_mailer.raise_delivery_errors = true
 end
